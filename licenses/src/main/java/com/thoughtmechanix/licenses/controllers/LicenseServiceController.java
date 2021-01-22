3672057a41cd74a059c56c34c1fb463385601f3e
Melvin Koopmans <melvin.koopmans@gmail.com>
2021-01-22 20:24:08 +0100

    add configuration server service

--- licenses/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ licenses/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -9,15 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/v1/organizations/{organizationId}/licenses")
 public class LicenseServiceController {
-    @RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
-    public License getLicences(
-            @PathVariable("organizationId") String organizationId,
-            @PathVariable("licenseId") String licenseId
-    ) {
-        return new License()
-                .withId(licenseId)
-                .withProductName("Teleco")
-                .withLicenseType("Seat")
-                .withOrganizationId("TestOrg");
-    }
+
+  @RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
+  public License getLicences(
+      @PathVariable("organizationId") String organizationId,
+      @PathVariable("licenseId") String licenseId
+  ) {
+    return new License()
+        .withId(licenseId)
+        .withProductName("Teleco")
+        .withLicenseType("Seat")
+        .withOrganizationId("TestOrg");
+  }
 }
