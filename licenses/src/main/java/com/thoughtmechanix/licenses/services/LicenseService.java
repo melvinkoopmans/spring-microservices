e1c05adbe62090b5a98dcbb24ccc14f8a6390b04
Melvin Koopmans <melvin.koopmans@gmail.com>
2021-01-27 21:23:55 +0100

    trigger hystrix with random sleep

--- licenses/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
+++ licenses/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
@@ -1,9 +1,11 @@
 package com.thoughtmechanix.licenses.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.thoughtmechanix.licenses.config.ServiceConfig;
 import com.thoughtmechanix.licenses.model.License;
 import com.thoughtmechanix.licenses.repository.LicenseRepository;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,12 +24,32 @@ public class LicenseService {
     return license.withComment(config.getExampleProperty());
   }
 
+  @HystrixCommand
   public List<License> getLicenseByOrg(String organizationId) {
+    randomlyRunLong();
+
     return licenseRepository.findByOrganizationId(organizationId);
   }
 
   public void saveLicense(License license) {
     license.withId(UUID.randomUUID().toString());
     licenseRepository.save(license);
+  }
+
+  /**
+   * Randomly run long to induce a timeout. Used for testing Hystrix behavior.
+   */
+  private void randomlyRunLong() {
+    Random rand = new Random();
+    int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+    if (randomNum==3) sleep();
+  }
+
+  private void sleep() {
+    try {
+      Thread.sleep(11000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
