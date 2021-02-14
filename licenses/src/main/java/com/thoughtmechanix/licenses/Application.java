f20c3ccd06ad7ef54df350312a5c103979f48d0b
Melvin Koopmans <melvin.koopmans@gmail.com>
2021-02-14 17:06:36 +0100

    move rest template to shared configuration

--- licenses/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ licenses/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -5,11 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,12 +14,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableCircuitBreaker
 @Import(SharedConfiguration.class)
 public class Application {
-
-  @LoadBalanced
-  @Bean
-  public RestTemplate getRestTemplate() {
-    return new RestTemplate();
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
