af2f7e3384f8966cb76643890498bcfa6946db8f
Melvin Koopmans <melvin.koopmans@gmail.com>
2021-02-15 08:58:12 +0100

    protect organization service with oauth2

--- organizations/src/main/java/com/thoughtmechanix/organizations/Application.java
+++ organizations/src/main/java/com/thoughtmechanix/organizations/Application.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableResourceServer
 @Import(SharedConfiguration.class)
 public class Application {
 
