d62ce17fe849cfb7c25b587a9dc2ed212ebccb30
Melvin Koopmans <melvin.koopmans@gmail.com>
2021-02-15 08:39:01 +0100

    configure dummy users

---
+++ authentication/src/main/java/com/thoughtmechanix/authentication/security/WebSecurityConfigurer.java
@@ -0,0 +1,36 @@
+package com.thoughtmechanix.authentication.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+  @Override
+  @Bean
+  public AuthenticationManager authenticationManagerBean() throws Exception  {
+    return super.authenticationManagerBean();
+  }
+
+  @Override
+  @Bean
+  public UserDetailsService userDetailsServiceBean() throws Exception {
+    return super.userDetailsServiceBean();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("melvin@test.com")
+        .password("password123")
+        .roles("ADMIN")
+        .and()
+        .withUser("user@test.com")
+        .password("password456")
+        .roles("USER");
+  }
+}
